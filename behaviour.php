81097067262b1c9356297c297f092480acc68469
Dr Bean <drbean@freeshell.org>
2017-07-15 11:13:32 +0800

    https://docs.moodle.org/dev/Question_behaviours#Question_behaviour_class

---
+++ behaviour.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Award a group grade, ie the average of the scores of all members of the group in which the student is a member
+ *
+ * @package    qbehaviour
+ * @subpackage group
+ * @copyright  2017 Dr Bean
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Question behaviour for group.
+ *
+ * Award a group grade, ie the average of the scores of all members of the group in which the student is a member
+ *
+ * @copyright  2017 Dr Bean
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qbehaviour_mybehaviour extends question_behaviour {
+    // ...
+}
